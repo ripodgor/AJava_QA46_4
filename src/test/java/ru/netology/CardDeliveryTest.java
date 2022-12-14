274159a69d53cd098b434319a8972e9e3771e0c3
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-12-14 18:32:17 +0300

    Updated files

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -9,8 +9,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
@@ -20,42 +20,42 @@ public class CardDeliveryTest {
     }
 
     @Test   // №1
-    public void shouldBeSuccessfullyCompleted() {
+    public void shouldBeSuccessfullyCompleted1() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Волгоград");
-        String verificationDate = generateDate(7, "dd.MM.yyyy");
+        String generateDate = generateDate(7, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys(verificationDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate);
         $("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
         $("[data-test-id='phone'] input").setValue("+79019012121");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $("[data-test-id=notification]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate));
+                .shouldHave(text("Успешно! Встреча успешно забронирована на " + generateDate));
     }
 
     @Test   // №2
-    public void shouldSuccessfulFormSubmissionAfterInteractingWithComplexElements() {
+    public void shouldBeSuccessfullyCompleted2() {
         open("http://localhost:9999");
         String city = "Волгоград";
+        int dayToAdd = 7;
+        int defaultAddedDays = 3;
         $("[data-test-id=city] input").setValue(city.substring(0, 2));
-        $(Selectors.byText("Волгоград")).click();
+        $$(".menu-item__control").findBy(text(city)).click();
         $("[data-test-id=date] input").click();
-        int days = 4;
-        for (int cycle = 0; cycle < days; cycle++) {
-            $(".calendar").sendKeys(Keys.ARROW_RIGHT);
+        if (!generateDate(defaultAddedDays, "MM").equals(generateDate(dayToAdd, "MM"))) {
+            $("[data-step=1']").click();
         }
-        $(".calendar").sendKeys(Keys.ENTER);
+        $$(".calendar__day").findBy(text(generateDate(dayToAdd, "d"))).click();
         $("[data-test-id=name] input").setValue("Иванов-Петров Иван");
         $("[data-test-id=phone] input").setValue("+79019012121");
         $("[data-test-id=agreement]").click();
-        $(".button").shouldHave(Condition.text("Забронировать")).click();
-        String verificationDate = LocalDate.now().plusDays(7)
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("button.button").click();
         $("[data-test-id=notification]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate));
+                .shouldHave(text("Успешно! Встреча успешно забронирована на " + generateDate(dayToAdd, "dd.MM.yyyy")));
+
     }
 }
 
